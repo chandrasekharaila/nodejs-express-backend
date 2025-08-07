89de0389be32c5812c58022034446bfa7e8822d6
Chandra Sekhar Aila <tarun@Vamsis-Mac-mini.local>
2025-08-07 18:51:40 +0530

    async handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    const server = app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server running on port ${process.env.PORT || 8080}`);
+    });
+    server.on("error", (error) => {
+      console.log("failed to start server, ", error);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed ", error);
+  });
