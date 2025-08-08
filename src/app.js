16e63d298af505da684440823c1dc1b8c27ce08a
Chandra Sekhar Aila <tarun@Vamsis-Mac-mini.local>
2025-08-08 12:27:31 +0530

    asynchanlder and errorhandler

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import apiErrorHandler from "./middlewares/apiErrorHandler";
 const app = express();
 
 app.use(
@@ -12,4 +13,5 @@ app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
 
+app.use(apiErrorHandler);
 export default app;
