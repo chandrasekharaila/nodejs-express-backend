16d01b315ec1fa4ac959744db11336b7ebae0b3e
Chandra Sekhar Aila <tarun@Vamsis-Mac-mini.local>
2025-08-08 12:31:38 +0530

    correcting exports

--- src/middlewares/apiErrorHandler.js
+++ src/middlewares/apiErrorHandler.js
@@ -5,4 +5,4 @@ const apiErrorHandler = (err, req, res, next) => {
   });
 };
 
-module.exports = apiErrorHandler;
+export { apiErrorHandler };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,4 +2,4 @@ const asyncHandler = (controller) => (req, res, next) => {
   Promise.resolve(controller(req, res, next)).catch(next);
 };
 
-module.exports = asyncHandler;
+export { asyncHandler };
