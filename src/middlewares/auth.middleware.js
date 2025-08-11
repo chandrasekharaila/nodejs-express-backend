7ba445c5dfa9c48fc3b875af0f87dab67241f6b8
Chandra Sekhar Aila <tarunaila007@gmail.com>
2025-08-11 12:56:12 +0530

    auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,33 @@
-import { User } from "../models/user.models,js";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
 
-const verifyJWT = asyncHandler(async (params) => {});
+const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.headers("authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    const decodedToken = jwt.decode(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = User.findById(decodedToken?._id).select(
+      "-password refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError("Invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(400, "Invalid Access Token");
+  }
+});
 
 export { verifyJWT };
